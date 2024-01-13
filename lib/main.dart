368a67ffdd8cf0cac63fe44e673fd23471740616
Cash Egley <skybl0ckerdev@gmail.com>
2024-01-13 14:16:44 -0500

    scouting commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-// ignore_for_file: avoid_unnecessary_containers, avoid_print, unused_import
+// ignore_for_file: avoid_unnecessary_containers, avoid_print, unused_import, unnecessary_import
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'navbar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'sp.dart';
 import 'package:gap/gap.dart';
 import 'variables.dart' as v;
+import 'package:flutter/animation.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
 
 void main() {
   print(v.reorganizePD(v.pageData));
@@ -16,19 +19,29 @@ class ScoutingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Scouting',
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (context) => const ScoutingHomePage(
               title: '',
             ),
-        '/analytics': (context) => const AnalyticsPage(
-              title: '',
-            ),
         '/scouting': (context) => const ScoutingPage(
               title: '',
             ),
+        '/auto': (context) => const AutoPage(
+              title: ''
+            ),
+        '/teleop': (context) => const TeleopPage(
+              title: ''
+            ),
+        '/endgame': (context) => const EndgamePage(
+              title: ''
+            ),
         '/schedule': (context) => const SchedulePage(
+              title: '',
+            ),
+        '/analytics': (context) => const AnalyticsPage(
               title: '',
             ),
         '/pitscouting': (context) => const PitScoutingPage(
@@ -93,6 +106,7 @@ class _ScoutingHomePageState extends State<ScoutingHomePage> {
                 setPref('qpint', 'oqeihtqoiw', v.pageData);
                 Navigator.pushNamed(context, '/scouting');
               },
+              
               style: TextButton.styleFrom(
                 textStyle: const TextStyle(fontSize: 40),
                 padding: const EdgeInsets.only(
@@ -104,7 +118,7 @@ class _ScoutingHomePageState extends State<ScoutingHomePage> {
               child: const Text(
                 "Scouting",
                 style: TextStyle(color: Colors.white),
-              ),
+              ).animate().fade(delay: 500.ms),
             ),
             const SizedBox(
               height: 20,
@@ -124,7 +138,7 @@ class _ScoutingHomePageState extends State<ScoutingHomePage> {
               child: const Text(
                 "Schedule",
                 style: TextStyle(color: Colors.white),
-              ),
+              ).animate().fade(delay: 700.ms),
             ),
             const SizedBox(
               height: 20,
@@ -144,7 +158,7 @@ class _ScoutingHomePageState extends State<ScoutingHomePage> {
               child: const Text(
                 "Analytics",
                 style: TextStyle(color: Colors.white),
-              ),
+              ).animate().fade(delay: 900.ms),
             ),
             const SizedBox(
               height: 20,
@@ -164,7 +178,7 @@ class _ScoutingHomePageState extends State<ScoutingHomePage> {
               child: const Text(
                 "Pit Scouting",
                 style: TextStyle(color: Colors.white),
-              ),
+              ).animate().fade(delay: 1100.ms),
             ),
             const SizedBox(
               height: 20,
@@ -186,7 +200,7 @@ class _ScoutingHomePageState extends State<ScoutingHomePage> {
               child: const Text(
                 "Super Scouting",
                 style: TextStyle(color: Colors.white),
-              ),
+              ).animate().fade(delay: 1350.ms),
             ),
             const SizedBox(
               height: 20,
@@ -206,8 +220,11 @@ class ScoutingPage extends StatefulWidget {
 }
 
 class _ScoutingPageState extends State<ScoutingPage> {
+    final List<bool> selectedStart = <bool>[false, false, false];
   @override
   Widget build(BuildContext context) {
+    TextEditingController matchNum = TextEditingController();
+    TextEditingController robotNum = TextEditingController();
     return Scaffold(
         drawer: const NavBar(),
         appBar: AppBar(
@@ -252,6 +269,7 @@ class _ScoutingPageState extends State<ScoutingPage> {
             style: TextStyle(color: Colors.white, fontSize: 20),
           ),
           TextField(
+            controller: robotNum,
               style: const TextStyle(fontSize: 20),
               decoration: InputDecoration(
                 filled: true,
@@ -268,6 +286,7 @@ class _ScoutingPageState extends State<ScoutingPage> {
             style: TextStyle(color: Colors.white, fontSize: 20),
           ),
           TextField(
+            controller: matchNum,
               style: const TextStyle(fontSize: 20),
               decoration: InputDecoration(
                 filled: true,
@@ -278,6 +297,26 @@ class _ScoutingPageState extends State<ScoutingPage> {
                 hintText: 'ex: 1',
               )
               ),
+              const Gap(25),
+              ElevatedButton(
+              onPressed: () {
+                print(robotNum.text);
+                print(matchNum.text);
+                Navigator.pushNamed(context, '/auto');
+              },
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: 40,
+                ),
+                padding: const EdgeInsets.only(
+                    left: 14, top: 12, right: 14, bottom: 12),
+                backgroundColor: Colors.blue,
+                side: const BorderSide(
+                    width: 3, color: Color.fromRGBO(65, 104, 196, 1)),
+              ), child: const Text("Confirm",
+              style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+              )
         ]
         )
       )
@@ -285,54 +324,19 @@ class _ScoutingPageState extends State<ScoutingPage> {
   }
 }
 
-class MatchNumPage extends StatefulWidget {
-  const MatchNumPage({super.key, required this.title});
-  final String title;
-  @override
-  State<MatchNumPage> createState() => _MatchNumPageState();
-}
-class _MatchNumPageState extends State<MatchNumPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: const NavBar(),
-        appBar: AppBar(
-          leading: Builder(
-          builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(
-                  Icons.menu,
-                  color: Color.fromRGBO(165, 176, 168, 1),
-                  size: 50,
-                ),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
-          ),
-          actions: [
-            Container(
-                child: IconButton(
-                    onPressed: () => Navigator.pushNamed(context, '/'),
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Color.fromRGBO(165, 176, 168, 1),
-                      size: 50,
-                    )))
-          ],
-          backgroundColor: const Color.fromRGBO(65, 68, 74, 1),
-          title: Image.asset(
-            'assets/images/rohawktics.png',
-            width: 75,
-            height: 75,
-            alignment: Alignment.center,
-          ),
-        )
-      );
-  }
-}
+const List<Widget> autoScoring = <Widget>[
+  Text('None'),
+  Text('Cargo'),
+  Text('Scored')
+];
+
+const List<Widget> communityLeave = <Widget>[
+  Text('None'),
+  Text('Inside'),
+  Text('Left')
+];
+
+
 
 class AutoPage extends StatefulWidget {
   const AutoPage({super.key, required this.title});
@@ -341,6 +345,8 @@ class AutoPage extends StatefulWidget {
   State<AutoPage> createState() => _AutoPageState();
 }
 class _AutoPageState extends State<AutoPage> {
+  bool toggleButton1 = false;
+    final List<bool> selectedStart = <bool>[false, false, false];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -364,7 +370,7 @@ class _AutoPageState extends State<AutoPage> {
           actions: [
             Container(
                 child: IconButton(
-                    onPressed: () => Navigator.pushNamed(context, '/'),
+                    onPressed: () => Navigator.pushNamed(context, '/scouting'),
                     icon: const Icon(
                       Icons.arrow_back,
                       color: Color.fromRGBO(165, 176, 168, 1),
@@ -378,8 +384,36 @@ class _AutoPageState extends State<AutoPage> {
             height: 75,
             alignment: Alignment.center,
           ),
+        ),
+        body: Center(
+        child: Column(children: <Widget>[
+          const Text("Auto Scoring", style: TextStyle(color: Colors.white, fontSize: 25),)
+          ,
+          ToggleButtons(
+            onPressed: (int index) {
+              setState(() {
+                for (int i = 0; i < selectedStart.length; i++) {
+                  selectedStart[i] = i == index; //CHECK AND MAKE SURE IT DOES WHAT IT SHOULD
+                }
+              }
+              );
+            },
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          selectedBorderColor: const Color.fromRGBO(65, 104, 196, 1), borderWidth: 2.5,
+          selectedColor: Colors.black,
+          fillColor: Colors.blue,
+          color: Colors.white,
+          constraints: const BoxConstraints(
+          minHeight: 40.0,
+          minWidth: 80.0,
+          ),
+          isSelected: selectedStart, // MAKE A NEW ONE OF THESE
+           children: autoScoring, //MAKE A NEW ONE OF THESE
+         ),
+        ]
         )
-      );
+      )
+    );
   }
 }
 
--- lib/variables.dart
+++ lib/variables.dart
@@ -5,14 +5,14 @@ dynamic pageData = {
   "autoPickup": 0, //0 not picked up, 1 picked up
   "floorPickup": 0, //0 not picked up, 1 picked up
   "feederPickup": 0, //0 not picked up, 1 picked up
-  1: 0, //0 not picked up, 1 picked up
-  2: 0, //0 not picked up, 1 picked up
-  3: 0, //0 not picked up, 1 picked up
-  4: 0, //0 not picked up, 1 picked up
-  5: 0, //0 not picked up, 1 picked up
-  6: 0, //0 not picked up, 1 picked up
-  7: 0, //0 not picked up, 1 picked up
-  8: 0, //0 not picked up, 1 picked up
+  "1": 0, //0 not picked up, 1 picked up
+  "2": 0, //0 not picked up, 1 picked up
+  "3": 0, //0 not picked up, 1 picked up
+  "4": 0, //0 not picked up, 1 picked up
+  "5": 0, //0 not picked up, 1 picked up
+  "6": 0, //0 not picked up, 1 picked up
+  "7": 0, //0 not picked up, 1 picked up
+  "8": 0, //0 not picked up, 1 picked up
   "speakerPlacement": 0, //0 0 notes, 1 1 note, 2 2 notes, 3 3 notes, 4 4 notes
   "ampPlacement": 0, //0 0 notes, 1 1 note, 2 2 notes, 3 3 notes, 4 4 notes
   "stagePlacement": 0, //0 0 notes, 1 1 note, 2 2 notes, 3 3 notes
@@ -20,6 +20,13 @@ dynamic pageData = {
       0, //0 = no hang,1 hang on one closest to field,2 hang on one closest to amp,3 hang on other one
   "microphonePlacement": 0, //0 = not landed, 1 landed
 };
+
+Map<dynamic,int> robotData = {
+  "robotNum": 0,
+  "matchNum": 0,
+};
+
 dynamic reorganizePD(dynamic data) {
   return pageData;
 }
+
