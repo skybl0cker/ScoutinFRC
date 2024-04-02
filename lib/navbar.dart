27c15bf25c74fa5371cbda80d20918738f0ecc2b
skybl0cker <skybl0ckerdev@gmail.com>
2024-04-02 08:35:58 -0400

    added account deletion

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, avoid_print
 
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,6 +6,23 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:scouting2024/auth_gate.dart' as auth;
 import 'package:scouting2024/sp.dart';
+
+Future<void> deleteFirebaseAccount() async {
+  try {
+    // Get the currently signed-in user
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      // Delete the user
+      await user.delete();
+      print('User account deleted successfully');
+    } else {
+      print('No user signed in');
+    }
+  } catch (e) {
+    print('Failed to delete user account: $e');
+  }
+}
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
@@ -26,6 +43,44 @@ class NavBar extends StatelessWidget {
             textColor: Colors.white,
             onTap: () {
               FirebaseAuth.instance.signOut();
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_remove),
+            iconColor: Colors.white,
+            title: const Text('Delete Account'),
+            textColor: Colors.white,
+            onTap: () {
+              showDialog(
+  context: context,
+  builder: (BuildContext context) {
+    return AlertDialog(
+      title: const Text('Delete your Account?'),
+      content: const Text(
+'''If you select Delete we will delete your account on our server.
+
+Your app data will also be deleted and you won't be able to retrieve it.
+
+Since this is a security-sensitive operation, you eventually are asked to login before your account can be deleted.'''),
+      actions: [
+        TextButton(
+          child: const Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: const Text(
+            'Delete',
+          ),
+          onPressed: () async {
+            deleteFirebaseAccount();
+          },
+        ),
+      ],
+    );
+  },
+);
             },
           ),
           ListTile(
